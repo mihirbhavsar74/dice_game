2d03fc8e4878d21947fd0fce313ff9f7a893c270
mihirbhavsar74 <86587310+mihirbhavsar74@users.noreply.github.com>
2022-07-15 11:57:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1=Math.floor(Math.random()*6 +1);
 var randomDiceImage="dice"+randomNumber1+".png";
-var randomImageSource="images/"+randomDiceImage;
+var randomImageSource="/images/"+randomDiceImage;
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 var randomNumber2=Math.floor(Math.random()*6+1);
 var randomDiceImage2="dice"+randomNumber2+".png";
-var randomImageSource2="images/"+randomDiceImage2;
+var randomImageSource2="/images/"+randomDiceImage2;
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImageSource2);
 if(randomNumber1 > randomNumber2){
